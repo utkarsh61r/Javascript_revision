1b1b93bb5d4351acc0effd7c44e4322d86adfdaf
Utkarsh Upadhyay <upadhyayutkarsh1614@gmail.com>
2026-02-11 23:02:10 +0530

    day 1 complete

--- day1.js
+++ day1.js
@@ -1,7 +1,14 @@
 // Day 1 JavaScript Revision
 
-function reverseString(str) {
-  return str.split("").reverse().join("");
-}
+// function reverseString(str) {
+//   return str.split("")
+  
+//   .reverse().join("");
+// }
 
-console.log(reverseString("JavaScript"));
+// console.log(reverseString("JavaScript"));
+const numbers = [1, 2, 3, 4, 5];
+
+const sum = numbers.reduce((total, num) => total + num, 0);
+
+console.log(sum); // 15
